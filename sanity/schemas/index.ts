062a1b33cde31ce2f0bb62837cde104b9c94db54
hrk330 <hrk3341@gmail.com>
2024-07-26 20:17:20 +0500

    first commit

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,20 @@
+import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import test from './test'
+
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [
+  test,
+  experiences,
+  workExperience,
+  works,
+  skills,
+  contact,
+  brands,
+  abouts,
+]
